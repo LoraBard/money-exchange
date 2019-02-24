453aae330f9bb4ccb3c5d779467ac939ad039b3a
BardLora <bard.lora@mail.ru>
2019-02-24 07:51:21 +0300

    feat:function which determines the minimum number of coins to make change for a given amount of money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let obj={
+        "H":50,
+        "Q": 25,
+        "D": 10,
+        "N": 5,
+        "P": 1
+    },obj2={},i=0;
+    if(currency>10000)
+    {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    if(currency<=0)
+    {
+        return {};
+    }
+    for(let key in obj)
+    {
+        i=~~(currency/obj[key]);
+        if(i!==0)
+        {
+            currency-=(i*obj[key]);
+            obj2[key]=i;
+        }
+    }
+    return obj2;
 }
